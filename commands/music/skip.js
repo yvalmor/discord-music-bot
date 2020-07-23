bfc5e0b503182dd43c1a403405ea2457d19533d3
Yvon Morice <yvon.morice@epita.fr>
2020-07-23 23:14:42 +0200

    added skip command

---
+++ commands/music/skip.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SkipToCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'skip',
+            memberName: 'skip',
+            group: 'music',
+            aliases: ['next'],
+            description:
+                'Skip one song',
+            guildOnly: true,
+        });
+    }
+
+    run(message) {
+
+        var voiceChannel = message.member.voice.channel;
+
+        if (!voiceChannel) return message.reply('Join a channel and try again');
+
+        if (
+            typeof message.guild.musicData.songDispatcher == 'undefined' ||
+            message.guild.musicData.songDispatcher == null
+        ) {
+            return message.reply('There is no song playing right now!');
+        }
+
+        if (message.guild.musicData.queue < 1)
+            return message.say('There are no songs in queue');
+
+        message.guild.musicData.queue.splice(0, 1);
+        message.guild.musicData.songDispatcher.end();
+        return;
+    }
+};

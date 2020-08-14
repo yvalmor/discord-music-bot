e28ad2f5d8422bf70c1bc3029c910107cb9b9835
Yvon Morice <yvon.morice@epita.fr>
2020-08-14 22:07:38 +0200

    correction on playnow command

--- commands/music/playnow.js
+++ commands/music/playnow.js
@@ -1,4 +1,9 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const Youtube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const { API_key: youtubeAPI } = require('../../config.json');
+const youtube = new Youtube(youtubeAPI);
 
 module.exports = class PlayCommand extends Command {
     constructor(client) {

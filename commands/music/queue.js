7f8ba949eb97c5567d6cfcd3515b6593716fb705
Yvon Morice <yvon.morice5@gmail.com>
2020-11-26 19:22:52 +0100

    test queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -9,17 +9,36 @@ module.exports = class QueueCommand extends Command {
             group: 'music',
             memberName: 'queue',
             guildOnly: true,
-            description: 'Display the song queue'
+            description: 'Display the song queue',
+            args: [
+                {
+                    key: 'page',
+                    prompt: 'what page ?',
+                    type: 'integer',
+                    default: 1
+                }
+            ]
         });
     }
 
-    run(message) {
+    run(message, { page }) {
         if (message.guild.musicData.queue.length == 0)
             return message.say('There are no songs in queue!');
         const titleArray = [];
+
+        const nbPage = message.guild.musicData.queue.length / 10;
+        page = page > nbPage ? nbPage : page;
+
+        let min = 11 * (page - 1) - (page - 1);
+        let max = 10 * page;
+
+        console.log('queueSize: ' + (nbPage * 10));
+        console.log('min: ' + min);
+        console.log('max: ' + max);
+
         /* eslint-disable */
         // display only first 10 items in queue
-        message.guild.musicData.queue.slice(0, 10).forEach(obj => {
+        message.guild.musicData.queue.slice(min, max).forEach(obj => {
             titleArray.push(obj.title);
         });
         /* eslint-enable */
